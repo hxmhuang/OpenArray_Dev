faba17ef9b9860985c94aa64f081dff95fec6d5b
gwind <gwind@ooclab.com>
2019-08-16 11:43:30 +0800

    udpate build scripts for release 1.0.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,20 +4,21 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 ROOTDIR=$(cd "$SCRIPTPATH/.." && pwd)
 
-RELEASE=1.0.0
+RELEASE=1.0.1
 PACKAGE=openarray-${RELEASE}-linux-x86_64
 
 cd $ROOTDIR
 
+wget https://github.com/hxmhuang/OpenArray/archive/${RELEASE}.tar.gz
+tar xf ${RELEASE}.tar.gz
+cd OpenArray-${RELEASE}/
+
 # generate configure
-aclocal
-autoconf
-automake --add-missing
+# aclocal
+# autoconf
+# automake --add-missing
 
 # build
-./configure --prefix=${HOME}/${PACKAGE}
-make -j$(nproc)
-make install
 
 PNETCDF_DIR=/opt/openarray ./configure --prefix=${HOME}/${PACKAGE} --with-mpi=/usr/lib64/mpi/gcc/openmpi3
 make
