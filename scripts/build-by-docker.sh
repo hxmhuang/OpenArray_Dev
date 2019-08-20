cb24cca43b7a1e91eb8cfb95c292613c8f6004fc
gwind <gwind@ooclab.com>
2019-08-20 14:13:42 +0800

    增加 2 个编译发行版编译脚本

--- scripts/build-by-docker.sh
+++ scripts/build-by-docker.sh
@@ -4,11 +4,31 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 ROOTDIR=$(cd "$SCRIPTPATH/.." && pwd)
 
-BUILDER_IMAGE=openarray-builder:opensuse15
+unalias cp
+unalias rm
+
+OS=$1
+
+case $OS in
+"opensuse")
+    ;;
+"centos")
+    ;;
+"ubuntu")
+    ;;
+*)
+    echo "$OS was unsupported yet, please prepare from scratch, take care!"
+    exit 0
+    ;;
+esac
+
+BUILDER_IMAGE=openarray-builder:${OS}
 
 function build_builder() {
-    cd $ROOTDIR/scripts/docker-build/opensuse15
-    docker build -t $BUILDER_IMAGE . -f openmpi-builder.Dockerfile
+    cp pnetcdf-1.11.2.tar.gz $ROOTDIR/scripts/docker-build/${OS}
+    cd $ROOTDIR/scripts/docker-build/${OS}
+    docker build -t $BUILDER_IMAGE . -f builder.Dockerfile
+    rm pnetcdf-1.11.2.tar.gz
     cd -
 }
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,25 +5,82 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 ROOTDIR=$(cd "$SCRIPTPATH/.." && pwd)
 
 RELEASE=1.0.1
-PACKAGE=openarray-${RELEASE}-linux-x86_64
 
+function check_platform() {
+    if [ -f /etc/os-release ]; then
+        cat /etc/os-release  # for debug
+        . /etc/os-release
+        OS=$ID
+        VER=$VERSION_ID
+    else
+        OS=$(echo $(uname -s) | tr '[:upper:]' '[:lower:]')
+        VER=$(uname -r)
+    fi
+}
+
+function suse_prepare() {
+    echo "This is OpenSUSE"
+    PNETCDF_DIR=/opt/openarray ./configure --prefix=${HOME}/${PACKAGE} --with-mpi=/usr/lib64/mpi/gcc/openmpi3
+}
+
+function darwin_prepare() {
+    echo "Darwin is unsupported now !"
+}
+
+function centos_prepare() {
+    echo "This is CentOS"
+    LIBS=-lmpi_cxx PNETCDF_DIR=${HOME}/install ./configure --prefix=${HOME}/install --with-mpi=/usr/lib64/openmpi3
+}
+
+function ubuntu_prepare() {
+    echo "This is Ubuntu"
+    LIBS=-lmpi_cxx PNETCDF_DIR=${HOME}/install ./configure --prefix=${HOME}/install
+}
+
+if [ "$(uname -m)" -eq "x86_64"; then
+    BITS=64
+else
+    echo "This prepare script just support x86_64 yet."
+    exit 0
+fi
+
+check_platform
+echo $OS
+echo $VER
+
+# 包名称
+PACKAGE=openarray-${RELEASE}-${OS}-x86_64
+
+# 准备源码
 cd $ROOTDIR
-
 wget https://github.com/hxmhuang/OpenArray/archive/${RELEASE}.tar.gz
 tar xf ${RELEASE}.tar.gz
 cd OpenArray-${RELEASE}/
 
-# generate configure
-# aclocal
-# autoconf
-# automake --add-missing
 
-# build
+case $OS in
+"opensuse-leap")
+    suse_prepare
+    ;;
+"darwin")
+    darwin_prepare
+    ;;
+"centos")
+    centos_prepare
+    ;;
+"ubuntu")
+    ubuntu_prepare
+    ;;
+*)
+    echo "$OS $VER was unsupported yet, please prepare from scratch, take care!"
+    exit 0
+    ;;
+esac
 
-PNETCDF_DIR=/opt/openarray ./configure --prefix=${HOME}/${PACKAGE} --with-mpi=/usr/lib64/mpi/gcc/openmpi3
+
 make
 make install
 
 # archive release
-tar cvjf ${PACKAGE}.tar.bz2 -C $HOME ${PACKAGE}
+tar cvjf ${PACKAGE}.tar.bz2 -C ${HOME} ${PACKAGE}
 echo "==> Generate release ${PACKAGE}.tar.bz2"
